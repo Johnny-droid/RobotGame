efb70cc1bdc3bbf6c1ee19743695ce3cbc676faa
Joao Alves <up202007614@fe.up.pt>
2021-04-03 18:08:13 +0100

    Overview of Functions

--- MazeFunctions.cpp
+++ MazeFunctions.cpp
@@ -1,4 +1,48 @@
+vector<vector<char>> readFile(string filename) {
+    // extract file and convert into vector of vector type (MAZE_XX.txt)
 
-bool alwaysTrue() {
-    return true;
 }
+
+void showMaze(vector<vector<int>> v) { 
+    // prints out the maze (don't forget to replace numbers with R)
+
+}
+
+vector<vector<int>> getRobotsXY(vector<vector<char>> maze) {
+    // returns a vector with the robot coordinates
+}
+
+void updateRobotsXY(vector<vector<int>> &v, int x, int y) {
+    //updates the robots vector with new positions according to the new Human position
+
+}
+
+int checkGameOver(vector<vector<char>> maze) {
+    // check whether there is an 'h' or if there are no 'R' -> GAME OVER
+    // 0 - continue, 1 - robots win, 2 - hero/human wins
+
+}
+
+void GameOver(int x, int time) {
+    // check if it won, and if so
+    // ask the name
+    // sort the winning times
+
+}
+
+
+
+int menu() {
+    // returns 0 Exit, 1 Rules, 2 Play
+
+}
+
+void rules() {
+    // prints rules
+}
+
+void clean() {
+    // cleans terminal
+
+} 
+
