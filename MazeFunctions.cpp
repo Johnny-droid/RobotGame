16d53c995eb4c4b08c2742e9b882a1666e449887
Joao Alves <up202007614@fe.up.pt>
2021-04-07 11:59:40 +0100

    updateMaze, checkGameOver, GameOver (still in progress), other extra functions added

--- MazeFunctions.cpp
+++ MazeFunctions.cpp
@@ -80,11 +80,12 @@ vector<vector<int>> getRobotsXY(const vector<vector<char>> &maze) {
     return v;
 }
 
-void updateRobotsXY(vector<vector<int>> &v, int y, int x) {
+void updateRobotsXY(vector<vector<int>> &v, const vector<int> &h) {
     //updates the robots vector with new positions according to the new Human position
     // x and y - human    Rx and Ry - robot
     for (int i = 0; i < v.size(); i++) {
         int &Rx = v[i][1], &Ry = v[i][0];
+        int x = h[1], y = h[0];
         if (Rx == x && Ry < y) {
             Ry++;
         } else if (Rx == x && Ry > y) {
@@ -109,12 +110,29 @@ void updateRobotsXY(vector<vector<int>> &v, int y, int x) {
     }
 }
 
-void updateMaze(vector<vector<char>> &maze, vector<vector<int>> &v, int y, int x) {
+void updateMaze(vector<vector<char>> &maze, vector<vector<int>> &v, const vector<int> &h) {
     //updates the maze according to the new information provided by the user and
     // the calculations of the robots new positions
 
+    // remove the 'R's and the 'H' from the maze, since we are going to assign new positions to them
+    for (int i = 0; i < maze.size(); i++) {
+        for (int j = 0; j < maze[i].size(); j++) {
+            if (maze[i][j] == 'R' || maze[i][j] == 'H') {
+                maze[i][j] = ' ';
+            }
+        }
+    }
+
+    //check for robot collisions with fences
+    for (int i = 0; i < v.size(); i++) {
+        if (maze[v[i][0]][v[i][1]] == '*') {
+            maze[v[i][0]][v[i][1]] = 'r';
+            v.erase(v.begin() + i);
+            i--;
+        }
+    }
+
     //check for robots collisions (robots with equal coordinates)
-    // not tested yet!
     for (int i = 0; i < v.size()-1; i++) {
         bool equal_found = false;
         for (int j = i+1; j < v.size(); j++) {
@@ -130,27 +148,112 @@ void updateMaze(vector<vector<char>> &maze, vector<vector<int>> &v, int y, int x
             i--;
         }
     }
+
+    // redraw robots (think of joining with the loop below)
+    for (int i = 0; i < v.size(); i++) {
+        maze[v[i][0]][v[i][1]] = 'R';
+    }
+
     //check if the human is alive...
+    // killed by robot
+    for (int i = 0; i < v.size(); i++) {
+        if (v[i] == h) {
+            maze[h[0]][h[1]] = 'h';
+            break;
+        }
+    }
+    //killed by eletric fence
+    if (maze[h[0]][h[1]] == '*') {
+        maze[h[0]][h[1]] = 'h';
+    }
 
 }
-/*
-int checkGameOver(vector<vector<char>> maze) {
+
+int checkGameOver(const vector<vector<char>> &maze) {
     // check whether there is an 'h' or if there are no 'R' -> GAME OVER
     // 0 - continue, 1 - robots win, 2 - hero/human wins
+    for (int i = 0; i < maze.size(); i++) {
+        for (int j = 0; j < maze[i].size(); j++) {
+            if (maze[i][j] == 'R') {
+                return 0;
+            }
+            if (maze[i][j] == 'h') {
+                return 1;
+            }
+        }
+    }
+    return 2;
 
 }
 
-void GameOver(int x, int time) {
+void GameOver(int x, int time, string filename) {
     // check if it won, and if so
     // ask the name
     // sort the winning times
+    // not finished yet!!
+    if (x == 2) {
+        ifstream file;
+        file.open(filename);
+        if (!file.is_open()) {
+            cout << "File not found" << endl;
+        } else {
+            string winner = readName();
+            string line, player;
+            int time_players;
+            size_t pos;
+            getline(file, line);   // ignore the first 2 lines of the file
+            getline(file, line);   // Lines: (Player - Time) and (---------)
+            while (getline(file, line)) {
+                player = line.substr(0, 15);
+                pos = line.find('-');
+                time_players = (int) strip(line.substr(pos+1));   //strip time
+            }
 
+            file.close();
+        }
+    }
+}
+
+// sorting function to sort the times of the winners
+void bubbleSort(vector<int> &v) {
+    int aux;
+    bool change = true;
+    while (change) {
+        change = false;
+        for (int i = 0; i < v.size()-1; i++) {
+            if (v[i] > v[i+1] ) {
+                change = true;
+                aux = v[i];
+                v[i] = v[i+1];
+                v[i+1] = aux;
+            }
+        }
+    }
+}
+
+string readName() {
+    // reads the name of the player, but it can only have 15 characters!!!
+    // if smaller, fills the name with whitespace
+    string name;
+    do {
+        cout << "Enter your name: ";
+        cin >> name;
+        if ( name.length() > 15 || cin.fail()) {
+            cin.clear();
+            cin.ignore(100000, '\n');
+            cout << "Invalid input!\n";
+        }
+    } while (name.length() > 15);
+
+    while (name.length() < 15) {
+        name += ' ';
+    }
+    return name;    
 }
 
 
 
-
-
+/*
 void rules() {
     // prints rules
 }
@@ -160,3 +263,10 @@ void clean() {
 
 } 
 */
+
+string strip(string &str) {
+    // removes the leading and trailing whitespaces of a string
+    size_t start = str.find_last_not_of(' ');
+    size_t end = str.find_last_not_of(' ');
+    return str.substr(start, end-start+1);
+}
