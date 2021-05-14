be64c78a8e6d960bf51015a6d5ecf1343e00bc7f
AntonioMatosGitHub <vaziosmellsbad@gmail.com>
2021-05-14 16:51:15 +0100

    notas extra

--- MazeGameOOP.cpp
+++ MazeGameOOP.cpp
@@ -9,3 +9,17 @@ int main() {
 
     
 }
+
+//menu 3) Winners, if empty say empty list, if not ask for maze number then show the list
+//eletrical posts +, you can go into them but you stay where you were and die
+//non eletrical posts * or robots R or r, you can't run into them
+//robots become r when they go to R or *, but stay dead in their place if they go to +
+//if H reaches O, then O becomes H and game ends
+//rules in RULES.txt
+// 
+//===========CLASSES======================================================
+//Post
+//Maze
+//Player
+//Robot
+//Game
