140e06c4ba5ccef3b447f796fa5c4d873fbf4b86
AntonioMatosGitHub <vaziosmellsbad@gmail.com>
2021-05-14 17:24:14 +0100

    classes exist yey

--- MazeClasses.cpp
+++ MazeClasses.cpp
@@ -7,3 +7,44 @@
 #include <cstdlib>
 using namespace std;
 
+class Post {
+    private:
+		bool eletric;
+		int coord_x;
+		int coord_y;
+};
+
+class Robot {
+	private:
+		bool alive;
+		int coord_x;
+		int coord_y;
+
+};
+
+class Player {
+    private:
+		bool alive;
+		int coord_x;
+		int coord_y;
+
+};
+
+class Maze {
+	private:
+		int dim_x;
+		int dim_y;
+		int coord_x_gate;
+		int coord_y_gate;
+		vector<Post> posts;
+
+};
+
+class Game {
+	private:
+		int time;
+		Maze maze;
+		Player player;
+		vector<Robot> robots;
+
+};
